d35ad7c050722ccd19d75c771d88c6cd3c26d72b
Tomas Kristek <tomas.kristek56@gmail.com>
2023-10-19 11:55:33 +0200

    cv04 - Done

--- cv04/Core/Src/main.c
+++ cv04/Core/Src/main.c
@@ -32,6 +32,14 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 #define ADC_Q 12
+
+/* Temperature sensor calibration value address */
+#define TEMP110_CAL_ADDR ((uint16_t*) ((uint32_t) 0x1FFFF7C2))
+#define TEMP30_CAL_ADDR ((uint16_t*) ((uint32_t) 0x1FFFF7B8))
+
+/* Internal voltage reference calibration value address */
+#define VREFINT_CAL_ADDR ((uint16_t*) ((uint32_t) 0x1FFFF7BA))
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -45,6 +53,7 @@ ADC_HandleTypeDef hadc;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
+static uint32_t button = 0;
 
 /* USER CODE END PV */
 
@@ -60,13 +69,37 @@ static void MX_ADC_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 static volatile uint32_t raw_pot;
+static volatile uint32_t raw_temp;
+static volatile uint32_t raw_volt;
 static uint32_t avg_pot;
+static uint8_t channel;
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 {
-	raw_pot = avg_pot >> ADC_Q;
-	avg_pot -= raw_pot;
-	avg_pot += HAL_ADC_GetValue(hadc);
+	switch (channel)
+	{
+		case 0:
+		{
+			raw_pot = avg_pot >> ADC_Q;
+			avg_pot -= raw_pot;
+			avg_pot += HAL_ADC_GetValue(hadc);
+		}
+		break;
+		case 1:
+		{
+			raw_temp = HAL_ADC_GetValue(hadc);
+		}
+		break;
+		case 2:
+		{
+			raw_volt = HAL_ADC_GetValue(hadc);
+		}
+		break;
+	}
+	if (__HAL_ADC_GET_FLAG(hadc, ADC_FLAG_EOS))
+		channel = 0;
+	else
+		channel++;
 }
 /* USER CODE END 0 */
 
@@ -104,6 +137,7 @@ int main(void)
   sct_init();
   HAL_ADCEx_Calibration_Start(&hadc);
   HAL_ADC_Start_IT(&hadc);
+  static enum { SHOW_POT, SHOW_VOLT, SHOW_TEMP } state = SHOW_POT;
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -113,7 +147,44 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	sct_value((raw_pot*(500+1))/4096, (raw_pot/(4096/9)));
+	if (!HAL_GPIO_ReadPin(GPIOC, S1_Pin))
+	{
+	  	button = HAL_GetTick();
+	  	state = SHOW_VOLT;
+	}
+	if (!HAL_GPIO_ReadPin(GPIOC, S2_Pin))
+	{
+	  	button = HAL_GetTick();
+	  	state = SHOW_TEMP;
+	}
+	if ((HAL_GetTick()-button) > 1000)
+	{
+	  	state = SHOW_POT;
+	}
+
+	switch (state)
+	{
+	  	case SHOW_POT:
+	  	{
+	  		sct_value((raw_pot*(500+1))/4096, ((raw_pot*9)/4096));
+	  	}
+	  	break;
+	  	case SHOW_VOLT:
+	  	{
+	  		uint32_t voltage = 330*(*VREFINT_CAL_ADDR)/raw_volt;
+	  		sct_value(voltage, 0);
+	  	}
+	  	break;
+	  	case SHOW_TEMP:
+	  	{
+	  		int32_t temperature = (raw_temp-(int32_t)(*TEMP30_CAL_ADDR));
+	  		temperature = temperature*(int32_t)(110-30);
+	  		temperature = temperature/(int32_t)(*TEMP110_CAL_ADDR-*TEMP30_CAL_ADDR);
+	  		temperature = temperature+30;
+	  		sct_value(temperature, 0);
+	  	}
+	  	break;
+	}
 	HAL_Delay(50);
   }
   /* USER CODE END 3 */
@@ -207,6 +278,22 @@ static void MX_ADC_Init(void)
   {
     Error_Handler();
   }
+
+  /** Configure for the selected ADC regular channel to be converted.
+  */
+  sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
+  if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel to be converted.
+  */
+  sConfig.Channel = ADC_CHANNEL_VREFINT;
+  if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
   /* USER CODE BEGIN ADC_Init 2 */
 
   /* USER CODE END ADC_Init 2 */
@@ -276,6 +363,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : S2_Pin S1_Pin */
+  GPIO_InitStruct.Pin = S2_Pin|S1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pin : LD2_Pin */
   GPIO_InitStruct.Pin = LD2_Pin;
